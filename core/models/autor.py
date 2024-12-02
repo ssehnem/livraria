ea994bbad1c4a5fbfaa6426dbb0c40194c77fbdf
ssehnem <sarasehnem84@gmail.com>
2024-12-02 09:46:51 -0300

    Customização do Admin

--- core/models/autor.py
+++ core/models/autor.py
@@ -3,6 +3,7 @@ from django.db import models
 class Autor(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.nome

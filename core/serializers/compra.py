3ee804594b69b907a1a0aa92c7340ff6c694eb99
ssehnem <sarasehnem84@gmail.com>
2024-12-09 08:35:08 -0300

    Inclusão do total da compra na listagem de compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -19,4 +19,4 @@ class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
     class Meta:
         model = Compra
-        fields = "__all__"
+        fields = ("id", "usuario", "status", "total", "itens")

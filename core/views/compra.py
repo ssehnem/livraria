fee1f28ccbdd7e6d5b6936bedce040136406b82f
ssehnem <sarasehnem84@gmail.com>
2024-12-16 09:16:14 -0300

    Finalizando a compra e atualizando a quantidade de itens em estoque

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,3 +1,8 @@
+from django.db import transaction
+
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Compra
@@ -22,3 +27,34 @@ class CompraViewSet(ModelViewSet):
         if self.action in ("create", "update"):
             return CompraCreateUpdateSerializer
         return CompraSerializer
+    
+    @action(detail=True, methods=["post"])
+    def finalizar(self, request, pk=None):
+        compra = self.get_object()
+
+        if compra.status != Compra.StatusCompra.CARRINHO:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"status": "Compra já finalizada"},
+            )
+
+        with transaction.atomic():
+            for item in compra.itens.all():
+
+                if item.quantidade > item.livro.quantidade:
+                    return Response(
+                        status=status.HTTP_400_BAD_REQUEST,
+                        data={
+                            "status": "Quantidade insuficiente",
+                            "livro": item.livro.titulo,
+                            "quantidade_disponivel": item.livro.quantidade,
+                        },
+                    )
+
+                item.livro.quantidade -= item.quantidade
+                item.livro.save()
+
+            compra.status = Compra.StatusCompra.FINALIZADO
+            compra.save()
+
+        return Response(status=status.HTTP_200_OK, data={"status": "Compra finalizada"})
